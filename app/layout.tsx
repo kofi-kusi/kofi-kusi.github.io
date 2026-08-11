7ff66cb1721882b6d348fc5bac9834cc3e65f0b2
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-08-11 08:44:47 +0000

    Update body class for responsive layout adjustments

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
         ibmPlexMono.className,
       )}
     >
-      <body className="antialiased max-w-xl my-8 mx-auto">
+      <body className="antialiased max-w-9/10 md:max-w-xl my-8 mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
